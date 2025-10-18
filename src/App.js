dbda3883b4f2e3e3cb907cb695c11ee9452775ad
soham4204 <psoham104@gmail.com>
2025-10-18 22:56:40 +0530

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🎉 Birthday Wish Platform (CRA) Setup Complete! 🎉
+      </h1>
     </div>
   );
 }
